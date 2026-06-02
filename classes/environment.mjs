79cc65e9c0e70b70cfb803d1ccfcba1f6f0b66af
Vivian Lampman <vivian.lampman@gmail.com>
2026-06-02 03:56:18 -0500

    Move environment object stub generation into new Environment static class (placeholder)

---
+++ classes/environment.mjs
@@ -0,0 +1,24 @@
+import { forage, water } from "../definitions/names.mjs";
+
+export default class Environment {
+  static generate(biome) {
+    return {
+      biome: biome,
+      food: {
+        [forage.leaves]: 0,
+        [forage.grass]: 0,
+        [forage.seeds]: 0,
+        [forage.nuts]: 0,
+        [forage.fruit]: 0,
+        [forage.algae]: 0,
+        [forage.lichen]: 0,
+        [forage.wood]: 0,
+        [forage.carrion]: 0,
+      },
+      water: {
+        [water.fresh]: 0,
+        [water.salt]: 0,
+      },
+    };
+  }
+}
--- main.mjs
+++ main.mjs
@@ -6,32 +6,12 @@ import { runSim } from "./simulation.mjs";
 
 const species = Object.values(speciesDefinitions).map(def => new Species(def));
 
-const population = [];
-for (const s of species) {
-  population.push({
-    species: s,
-    count: s.getInitialPopulation().population,
-    fat: s.getInitialFatPercentage(),
-  });
-}
+const population = species.map(s => ({
+  species: s,
+  count: s.getInitialPopulation().population,
+  fat: s.getInitialFatPercentage(),
+}));
 
-const environment = {
-  biome: biome,
-  food: {
-    [forage.leaves]: 0,
-    [forage.grass]: 0,
-    [forage.seeds]: 0,
-    [forage.nuts]: 0,
-    [forage.fruit]: 0,
-    [forage.algae]: 0,
-    [forage.lichen]: 0,
-    [forage.wood]: 0,
-    [forage.carrion]: 0,
-  },
-  water: {
-    [water.fresh]: 0,
-    [water.salt]: 0,
-  },
-}
+const environment = Environment.generate(biome);
 
 runSim(environment, population, 10000);
